caa97eebc1b6f448c9d3e79e949dfb3a503139ae
matheus <matheusmw21@gmail.com>
2023-09-20 20:49:02 -0300

    feat: classe musica

---
+++ ScreenSoundOO/Musica.cs
@@ -0,0 +1,25 @@
+﻿class Musica 
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFicha()
+    {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        
+        if(disponivel)
+        { 
+            Console.WriteLine("Disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Disponível no plano plus");
+        }
+    }
+
+}
+
+
---
+++ ScreenSoundOO/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Musica musica1 = new Musica();
+
+musica1.nome = "Strawberry Fields Forever";
+musica1.artista = "The Beatles";
+musica1.duracao = 400;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Man in the box";
+musica2.artista = "Alice in Chains";
+musica2.duracao = 234;
+musica2.disponivel = false;
+
+musica1.ExibirFicha();
+musica2.ExibirFicha();
+
+
+
